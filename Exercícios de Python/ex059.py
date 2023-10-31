d51f9426269b4b936e5e4ee4af27b7a8a9ac438d
Caio Marcio <101654117+Caio893@users.noreply.github.com>
2023-10-31 10:26:13 -0300

    Update ex059.py

--- Exercícios de Python/ex059.py
+++ Exercícios de Python/ex059.py
@@ -4,7 +4,7 @@ num2 = int(input('Digite o Segundo valor\n ->'))
 option = 9
       
 while option != 5:
-    print('[1] somar\n[2] multiplicar\n[3] maior\n[4]novos números\n[5] sair do programa')
+    print('[1] somar\n[2] multiplicar\n[3] maior\n[4] novos números\n[5] sair do programa')
     option = int(input('\n->'))
     if option == 1:
         soma = num1 + num2
@@ -18,9 +18,9 @@ while option != 5:
         maior = num1
         if num2 > num1:
             maior = num2
-        print(f'O maior número entre {num1} e {num2} é {maior}')
-    else:
-        print('Não tem maior os dois valores são iguais')
+            print(f'O maior número entre {num1} e {num2} é {maior}')
+        elif maior == num2:
+            print('Não tem maior os dois valores são iguais')
     
     if option == 4:
         num1 = int(input('Digite novos valores\n-> '))
@@ -29,7 +29,7 @@ while option != 5:
     if option == 5:
         print('Obrigado')
     
-    else:
+    if option != 1 and option != 2 and option != 3 and option != 4 and option != 5:
         print('Opção invalida tente novamente entre 1 e 5')
     
     
