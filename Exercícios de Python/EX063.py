d711335b3247c66b3f6b98dff506e3740374d456
Caio Marcio <101654117+Caio893@users.noreply.github.com>
2024-03-25 16:22:06 -0300

    atts

--- Exercícios de Python/EX063.py
+++ Exercícios de Python/EX063.py
@@ -2,7 +2,7 @@ print(f'Desafio 63'.center(64, '@'))
 termos = int(input('Qual o primeiro termos\n ->'))
 c = 0
 ultimo = 0
-penultimo =1
+penultimo = 1
 if termos == 1 or termos == 2:
     print(f'O valor é 1')
 else:
